75c32f76de1de5e5f66e3869309c2c85e076f3dc
Bree-codes <mwongera.mukami22@students.dkut.ac.ke>
2025-06-03 08:47:17 +0300

    python scripts for text preprocessing

--- Text-Preprocessing/lowercase_converter.py
+++ Text-Preprocessing/lowercase_converter.py
@@ -1,2 +1,11 @@
 # I am learning text preprocessing in NLP
 # lets write a simple python function that converts text to all lowercase using.lower() function
+
+def lowercase_converter(text):
+    return text.lower()
+
+input_txt = "Hello there, my name is BRENDA"
+output = lowercase_converter(input_txt)
+
+print(output)
+
