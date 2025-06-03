344139162afe9924f2d3437075d3f23111a7ac06
Bree-codes <mwongera.mukami22@students.dkut.ac.ke>
2025-06-03 09:59:21 +0300

    python scripts for text preprocessing

--- Text-Preprocessing/removing_stopwords.py
+++ Text-Preprocessing/removing_stopwords.py
@@ -1,17 +1,19 @@
 # function to remove stopwords
 
 import nltk
-nltk.download('stopwords')
-nltk.download('punkt')
-
 from nltk.corpus import stopwords
-stopwords = set(stopwords.words('english'))
-
 from nltk.tokenize import word_tokenize
+
+# Ensure downloads are handled
+nltk.download('punkt')
+nltk.download('stopwords')
+
 text = "She is learning Natural Language Processing and it is fun"
+
+# Tokenize and remove stopwords
 tokens = word_tokenize(text)
-
-filtered_tokens = [word for word in tokens if word.lower()not in stopwords]
-
+stop_words = set(stopwords.words('english'))
+filtered_tokens = [word for word in tokens if word.lower() not in stop_words]
 clean_text = " ".join(filtered_tokens)
-print(clean_text)
+
+print("Filtered Text:", clean_text)
